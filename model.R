80d4633d0d707b7946d1ed2b2ece0b83049a0f46
Matteo Abis <gmatteo..abis@gmail.com>
2017-07-03 19:14:26 +0200

    first calculation

---
+++ model.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+library(data.table)
+
+structure.factors.file = "structure_factors.csv"
+structure.factors = fread(structure.factors.file)
+spectrum = fread("spectrum.csv")
+thickness.map = fread("data/thickness-map-microct.csv")
+norm = 1 / spectrum[, sum(total_weight)]
+spectrum = spectrum[, total_weight := norm * total_weight]
+
+mu.total = function(dfec) {
+    logB = (spectrum[, total_weight] %*% dfec)
+    return(logB)
+}
+
+sf = structure.factors[, mu.total(dfec_lynch), by=diameter]
+setnames(sf, "V1", "mu.d")
+sf[, density := thickness.map[, density]]
+print(sf)
+mu = sf[,density %*% mu.d]
+print(mu)
+print(exp(-mu * 0.005))
+
